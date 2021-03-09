c0a008858c8e1b30be850519b55eaaaed426d1aa
Tumelo <tumie201511@gmail.com>
2021-03-09 23:10:35 +0200

    added a Task0.4.js file for checking odd and even numbers

---
+++ Task0.4.js
@@ -0,0 +1,18 @@
+/* 
+Checks if parameter number is even || odd.
+output: typeof(string)
+
+*/
+
+function evenOrOdd(number) {
+    if (typeof(number) === 'number'){
+        if (number % 2 == 0){
+            return "even";
+        }else{
+            return "odd";
+        }
+    }else{
+        return "Invalid Inputs!"
+    }
+    
+}
